e7857fe1b656e688f55a7d106fd230ae43ae7f3b
dn.kalabin <kalabin.danil28@mail.ru>
2022-12-16 18:52:21 +0300

    resolve

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,21 @@
-import { subscribe, unsubscribe } from './resources/API';
+import {useEffect, useState} from "react";
+import {subscribe, unsubscribe} from './resources/API';
 
 export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+	const [message, setMessage] = useState(-1);
+	const updateMessage = (message: any) => {
+		setMessage(message);
+	};
+	
+	useEffect(() => {
+		setMessage(-1);
+		subscribe(props.sourceId, updateMessage);
+		return () => unsubscribe(props.sourceId, updateMessage);
+	}, [props.sourceId]);
+	
+	return (
+		<div>
+			{props.sourceId}: {message}
+		</div>
+	);
 }
